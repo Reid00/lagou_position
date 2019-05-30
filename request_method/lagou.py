ef3606aae7549dcae8c403653d88d9740aa7b6c5
Reid00 <isreid.zhang@outlook.com>
2019-05-30 14:12:00 +0800

    add y value

--- request_method/lagou.py
+++ request_method/lagou.py
@@ -6,6 +6,7 @@ import random, os
 import pandas as pd
 import matplotlib.pyplot as plt
 from pylab import mpl
+import numpy as np
 
 
 # 获取存储职位信息的json对象，遍历获得公司名、福利待遇、工作地点、学历要求、工作类型、发布时间、职位名称、薪资、工作年限
@@ -62,21 +63,28 @@ def mk_plot():
     mpl.rcParams['font.sans-serif'] = ['SimHei']
     data = pd.read_csv('python_position.csv', sep=',', header=0, encoding='utf-8')
     position_cnt = data.groupby(by=['city']).size()  # 每个城市有多少职位
-    # plt.title('各个城市Python职位数目')
-    # plt.xlabel('城市名称')
-    # plt.ylabel('职位数目')
+    plt.title('各个城市Python职位数目')
+    plt.xlabel('城市名称')
+    plt.ylabel('职位数目')
     # position_cnt.plot.bar()
-    # plt.show()
+    x = position_cnt.index
+    y = position_cnt.values
+    plt.bar(x, y)
+    plt.show()
     salary_city = data.groupby(['city', 'education'])[
         'salary'].value_counts()  # 这时, 组内操作的结果不是单个值, 是一个序列, 我们可以用.unstack()将它展开
     # salary_mean = data.groupby('city')['salary'].value_counts().unstack()
     shanghai_salary = data[data['city'] == '上海']['salary'].value_counts(normalize=True)  # normalize=True 显示百分比
     education = data['education'].value_counts(normalize=True)
-
-    salary_city.plot.bar()
-    plt.show()
+    # salary_city.plot.bar()
+    # plt.show()
 
     # 添加数据标签
+    for a, b in zip(x, y):
+        plt.text(a, b + 0.05, r'{}'.format(b), ha='center', va='bottom', fontsize=11)
+    plt.ylim(0, 3700)
+    plt.show()
+    # x = len(position_cnt.index)
     # for a, b in zip(position_cnt.index, position_cnt['column0']):
     #     plt.text(a, b + 0.05, '%.0f' % b, ha='center', va='bottom', fontsize=7)
     # position_cnt.plot.bar()
